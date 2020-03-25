ac026789d84e8c21324682a196e264ab0e163523
Jinkyu Kim <jkyu126@gmail.com>
2020-03-25 21:31:25 +0900

    20200325

--- 04_project_based_learning/download.R
+++ 04_project_based_learning/download.R
@@ -15,7 +15,7 @@ bond_1yr <- tq_get("SHY", get="stock.prices", from=from)
 oil <- tq_get("DCOILWTICO", get = "economic.data", from=from)
 gold <- tq_get("GOLDAMGBD228NLBM", get = "economic.data", from=from)
 usd <- tq_get("EXKOUS", get="economic.data", from=from) #FROM 1981-04
-us_home <-tq_get("USSTHPI", get="economic.data", from=from)
+home <-tq_get("USSTHPI", get="economic.data", from=from)
 
 
 # Process data
@@ -26,13 +26,12 @@ kospi = kospi %>% mutate(price=adjusted, symbol="kospi") %>% select(symbol, date
 bond_20yr = bond_20yr %>% mutate(price=adjusted, symbol="bond_20yr") %>% select(symbol, date, price)
 bond_1yr = bond_1yr %>% mutate(price=adjusted, symbol="bond_1yr") %>% select(symbol, date, price)
 
+home = home %>% mutate(symbol = "us_home")
 oil = oil %>% mutate(symbol = "oil")
 gold = gold %>% mutate(symbol = "gold")
 usd = usd %>% mutate(symbol = "usd")
-us_home = us_home %>% mutate(symbol = "us_home")
 
-
-DF = rbind(sp500, djia, kospi, vix, oil, gold, usd, us_home, us_bond_10yr, us_tbill_90day)
+DF = rbind(sp500, djia, kospi, vix, oil, gold, usd, us_home, bond_20yr, bond_1yr)
 DF2 = DF %>% 
   mutate(time = paste0(year(date), 
                        ifelse(nchar(month(date))==1, 
@@ -51,20 +50,46 @@ DF3 = DF2 %>%
 DF4 = DF3 %>%
   mutate(year = year(time2), period = NULL) %>%
   mutate(period = ifelse(1998<=year & year<=2002, "Phase0", 
-                         ifelse(2004<=year & year<=2009, "Phase1",
+                         ifelse(2004<=year & year<=2008, "Phase1",
                                 ifelse(2010<=year & year<=2014, "Phase2",
                                        ifelse(2015<=year & year<=2019, "Phase3", "NOT_USED"))))) 
 
 DF5 = DF4 %>%
   filter(period %in% c("Phase0", "Phase1", "Phase2", "Phase3")) %>%
-  mutate(plusyear = ifelse(period=="Phase0", 18, 17)) %>%
+  mutate(plusyear = ifelse(period=="Phase0", 18, 
+                           ifelse(period=="Phase1", 17, 16))) %>%
   mutate(time3 = paste0(year(time2)+plusyear, 
                         ifelse(nchar(month(time2))==1, 
                                paste0("0", month(time2)), month(time2)), "01")) %>%
   mutate(time4 = as.Date(time3, format = "%Y%m%d"))
 
+names = c("djia", "gold", "kospi", "oil", "sp500", "bond_1yr",
+          "us_home", "bond_20yr", "usd", "vix")
+
 mydat = DF5 %>% mutate(time = time4, price = adjusted) %>%
-  select(symbol, time, price, period) 
+  select(symbol, time, price, period) %>% 
+  arrange(match(symbol, names), time)
+
+
+us_home = data.frame(year = rep(2016:2035, each=12), month = rep(1:12, 20)) %>%
+  mutate(time = paste(year, ifelse(nchar(month)==1, paste0("0", month), month), "01", sep="-")) %>% 
+  mutate(time = as.Date(time, format = "%Y-%m-%d")) %>%
+  select(time, year) %>%
+  left_join(mydat %>% filter(symbol=="us_home"), by="time") %>%
+  mutate(symbol = "us_home") 
+
+us_home$price[-c(239:240)] = na.approx(us_home$price)
+us_home$price[239:240] = us_home$price[238]
+
+us_home = us_home %>% mutate(period = ifelse(
+  year<=2020, "Phase0", 
+  ifelse(year>=2021 & year<=2025, "Phase1", 
+         ifelse(year>=2026 & year<=2030, "Phase2", "Phase3"))
+))
+mydat2 = mydat %>% filter(symbol != "us_home")
+us_home = us_home %>% select(symbol, time, price, period)
+mydat = rbind(mydat2, us_home) %>%
+  arrange(match(symbol, names), time)
 
 phase0 = mydat %>% filter(period == "Phase0")
 phase1 = mydat %>% filter(period == "Phase1")
